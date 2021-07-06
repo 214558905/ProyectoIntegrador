9413efe68698e39806f00f575d54f927698c2c3e
ales <alexnaula57@gmail.com>
2021-07-05 21:56:09 -0500

    Todos los controladores Creados

--- src/main/java/com/rafael/falconi/Api/controllers/NewsController.java
+++ src/main/java/com/rafael/falconi/Api/controllers/NewsController.java
@@ -1,5 +1,27 @@
 package com.rafael.falconi.Api.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.rafael.falconi.Api.entities.News;
+import com.rafael.falconi.Api.repositories.NewsRepository;
+
+@Controller
 public class NewsController {
 
+	private NewsRepository newsRepository;
+
+	@Autowired
+	public NewsController(NewsRepository newRepository) {
+		this.newsRepository = newRepository;
+	}
+
+	public List<News> findAllNews(){
+		
+		return this.newsRepository.findAll();
+	}
+
+	
 }
